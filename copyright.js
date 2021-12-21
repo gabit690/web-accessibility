17a66733aceae11ba441c8d9adadb885f9b6f280
gabit <gabit690@gmail.com>
2021-12-21 10:02:09 -0300

    Screen readers can detected relevant elements

--- copyright.js
+++ copyright.js
@@ -1,5 +1,5 @@
 document.getElementsByTagName("footer")[0].innerHTML = `
-    <p id="copyright">
+    <p id="copyright" tabindex="0">
         developed by <a id="author" href="#">gabit</a> <span id="year"></span>
     </p>`
 let currentYear = (new Date().getFullYear());
